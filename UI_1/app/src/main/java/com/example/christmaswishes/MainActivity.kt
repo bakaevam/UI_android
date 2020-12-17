924c7bf40356eab2bff2939fcf92d56bdf7666f6
Maria Bakaeva <mary396969@gmail.com>
2020-12-17 21:56:41 +0300

    version2

--- UI_1/app/src/main/java/com/example/christmaswishes/MainActivity.kt
+++ UI_1/app/src/main/java/com/example/christmaswishes/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.christmaswishes
 
-import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.renderscript.ScriptGroup
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -19,36 +13,114 @@ class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
     lateinit var img: ImageView
     lateinit var list: TextView
+    lateinit var title_text: TextView
     lateinit var button: Button
+    lateinit var button_clear: Button
+    private var isButtonPressed: Boolean = false
+    lateinit var fromEditText: String
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    companion object {
+        private const val TEXT_KEY = "TEXT_KEY"
+        private const val TEXT_TO_IMG_KEY = "TEXT_TO_IMG_KEY"
+        private const val TAG = "my logs"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editText = findViewById(R.id.edit_text)
         img = findViewById(R.id.imageView)
         list = findViewById(R.id.list)
+        title_text = findViewById(R.id.textView)
         button = findViewById(R.id.button)
+        button_clear = findViewById(R.id.button_clear)
 
-        list.setText("""
+        list.text = """
             Socks
             Sweets
             Christmas Tree
             Candles
             Fireworks
             Mandarins
-        """)
+        """
 
-       /* button.setOnClickListener{
-            list.isInvisible = true
-            img.isInvisible = false
+        button.setOnClickListener {
+            if (editText.text.isNotEmpty()) {
+                Log.d(TAG, "Repeat button")
+                list.isInvisible = true
+                img.isInvisible = false
+                isButtonPressed = true
 
-            var fromEditText = editText.text.toString()
+                fromEditText = editText.text.toString()
 
-            when(fromEditText) {
-                "Socks" -> img.setImageURI(Uri.parse("https:///cdn.shopify.com/s/files/1/1872/4935/products/4697951522_82857126_512x512.jpg?v=1548170941"))
+                when (fromEditText.toLowerCase()) {
+                    "socks" -> img.setImageResource(R.drawable.socks)
+                    "sweets" -> img.setImageResource(R.drawable.sweets)
+                    "christmas tree" -> img.setImageResource(R.drawable.tree)
+                    "candles" -> img.setImageResource(R.drawable.candles)
+                    "fireworks" -> img.setImageResource(R.drawable.fireworks)
+                    "mandarins" -> img.setImageResource(R.drawable.mandarins)
+                    else -> {
+                        img.isInvisible = true
+                        list.isInvisible = false
+                    }
+                }
+                title_text.text = "I want $fromEditText"
+                it.isEnabled = false
+                button_clear.isEnabled = true
             }
+        }
 
-        }*/
+        button_clear.setOnClickListener {
+            title_text.text = "I want ..."
+            button.isEnabled = true
+            it.isEnabled = false
+            editText.text.clear()
+            img.isInvisible = true
+            list.isInvisible = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(TEXT_KEY, title_text.text.toString())
+        outState.putBoolean("IS_BUTTON_PRESSED", isButtonPressed)
+        outState.putString(TEXT_TO_IMG_KEY, editText.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun restoreImage(text: String) {
+        Log.d(TAG, "restore Image")
+        list.isInvisible = true
+        img.isInvisible = false
+
+        when (text.toLowerCase()) {
+            "socks" -> img.setImageResource(R.drawable.socks)
+            "sweets" -> img.setImageResource(R.drawable.sweets)
+            "christmas tree" -> img.setImageResource(R.drawable.tree)
+            "candles" -> img.setImageResource(R.drawable.candles)
+            "fireworks" -> img.setImageResource(R.drawable.fireworks)
+            "mandarins" -> img.setImageResource(R.drawable.mandarins)
+            else -> {
+                img.isInvisible = true
+                list.isInvisible = false
+            }
+        }
+        isButtonPressed = false
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        lateinit var text: String
+        savedInstanceState.getString(TEXT_KEY)?.let {
+            title_text.text = it
+        }
+
+        if (savedInstanceState.getBoolean("IS_BUTTON_PRESSED", true)) {
+            savedInstanceState.getString(TEXT_TO_IMG_KEY)?.let {
+                text = it
+            }
+            restoreImage(text)
+        }
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
